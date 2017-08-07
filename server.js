d1094301b71eb6617322788ffaf87aad52344798
Rajkumar <30652521+rajkumarvelu@users.noreply.github.com>
2017-08-07 14:49:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article1', function(req,res){
+    res.send('article1 requested here ande serveed here')
+});
+app.get('/article2', function(req,res){
+    res.send('article2 requested here ande serveed here')
+});
+app.get('/article3', function(req,res){
+    res.send('article3 requested here ande serveed here')
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
