a90b301a94770920441bd134eb0ad7d3c923eba4
Rajkumar <30652521+rajkumarvelu@users.noreply.github.com>
2017-08-14 22:13:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ app.use(morgan('combined'));
 
 app.get('/counter',function (req,res) {
    counter=counter+1;
-   res.send(counter);
+   res.send(counter,tostring());
 });
 
+var counter=0;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
