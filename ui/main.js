fd3ec44268fee9f469302051dc05256b450ce1cb
Rajkumar <30652521+rajkumarvelu@users.noreply.github.com>
2017-08-16 21:41:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,18 @@
 //console.log('Loaded!');
 var button= document.getElementById('counter');
+var counter=0;
 
 button.onclick = function() {
     
-        var request = new XMLHttpRequest();
+    counter= counter+1;
+    
+    var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+    
+};
+
+    
+          /*  var request = new XMLHttpRequest();
     request.onreadystatechange = function()
     {
       if (request.readyState === XMLHttpRequest.DONE)
@@ -16,13 +25,10 @@ button.onclick = function() {
              
          }
       }
-
-    
-      
     };
     
           request.open('GET', 'http://velurajkumar.imad.hasura-app.io/counter',true);
     request.send(null);
 
     
-};
+};*/
