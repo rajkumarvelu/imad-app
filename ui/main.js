df57e2f09cceb6c750b6730a4f2ec059bceb42a1
Rajkumar <30652521+rajkumarvelu@users.noreply.github.com>
2017-08-16 21:32:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,5 @@
 //console.log('Loaded!');
 var button= document.getElementById('counter');
-var counter=0;
 
 button.onclick = function() {
     
@@ -9,11 +8,11 @@ button.onclick = function() {
     {
       if (request.readyState === XMLHttpRequest.DONE)
       {
-               if (request.Status === 200)
+               if (request.status === 200)
          {
              var counter = request.responseText;
              var span = document.getElementById('count');
-              span.innerHTML = count.toString();
+              span.innerHTML = counter.toString();
              
          }
       }
