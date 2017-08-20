dae37ed0f32fcfe7911f4c3a42697ebb66acb399
Rajkumar <30652521+rajkumarvelu@users.noreply.github.com>
2017-08-21 00:29:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 
-//var config = {
- // user:'velurajkumar',
- // database :'velurajkumar',
- // host: db.imad.hasura-app.io,
- // port: 5432,
- // password: process.env.DB_PASSWORD};
+var config = {
+ user:'velurajkumar',
+ database :'velurajkumar',
+  host: db.imad.hasura-app.io,
+  port: 5432,
+  password: process.env.DB_PASSWORD};
 
 var app = express();
 app.use(morgan('combined'));
