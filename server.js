e213b98fb06eac6969313484f797fcac19a1db49
Rajkumar <30652521+rajkumarvelu@users.noreply.github.com>
2017-08-22 23:10:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
  user:'velurajkumar',
@@ -20,6 +21,26 @@ app.get('/counter',function (req,res) {
    counter= counter + 1;
    res.send(counter.toString() );
 });
+
+app.get('/hash/:input', function(req,res){
+   
+   var hashedString = hash (req.params.input, 'this is random string');
+   res.send(hashedString);
+   
+});
+
+function hash (input,salt){
+    ///
+    //
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+    
+}
+
+
+
+
+
 
 var pool = new Pool(config);
 
