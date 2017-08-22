00e58d971431a4f670e037f24b8644d30a90ce01
Rajkumar <30652521+rajkumarvelu@users.noreply.github.com>
2017-08-22 23:11:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/hash/:input', function(req,res){
 function hash (input,salt){
     ///
     //
-    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,5,'sha512');
     return hashed.toString('hex');
     
 }
